3ada272640a13802b008a2c573cb0895053a2292
William Leung <w.leung.93@gmail.com>
2020-12-07 16:44:06 -0800

    tested apiUtil on window

--- frontend/heighten.jsx
+++ frontend/heighten.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 // import configureStore from './store/store';
 // import Root from './components/root';
 
+import { signup, login, logout } from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
     // const store = configureStore();
     const root = document.getElementById('root');
+    
+    //! testing
+    // window.login = login;
+    // window.signup = signup;
+    // window.logout = logout;
+    //! testing
+
     // ReactDOM.render(<Root store={store} />, root);
     ReactDOM.render(<h1>Testing Root</h1>, root);
 });
