e24d2cebf81bbc5f6573d554d690b4d88b636f0c
William Leung <w.leung.93@gmail.com>
2020-12-09 19:47:18 -0800

    more css updates

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -40,30 +40,65 @@ class SignUpForm extends React.Component {
 
     render() {
         return (
-            <div className='login_form_container'>
-                {this.renderErrors()}
-                <h1>Create New Customer Account</h1>
-                
-                <form onSubmit={this.handleSubmit} className='login_form_box'>
-                    <label>Email:
-                        <input type="text"
-                            value={this.state.email}
-                            onChange={this.handleInput('email')}
-                        />
-                    </label>
+            <div>
+                <span className='signup_title'>Create New Customer Account</span>
 
-                    <label>Password:
-                        <input type="password"
-                            value={this.state.password}
-                            onChange={this.handleInput('password')}
-                        />
-                    </label>
-                    <input type="submit" value="Create An Account" />
-                </form>
-                {this.props.formType === "login"
-                    ? <Link to='/signup'>Create an account</Link>
-                    : <Link to='/login' >Sign In</Link>
-                }`
+                <div className='signup_form_container'>
+                    {this.renderErrors()}
+
+                    <div className='left_signin'>
+                        <h2 className='sub_title'>Personal Information</h2>
+                        
+                        <form onSubmit={this.handleSubmit} className='signup_form_box'>
+                            <div className='personal_login'>
+                                <label className='name_signup'>First Name:
+                                    <input type="text"/>
+                                </label>
+
+                                <label className='lname_signup'>Last Name:
+                                    <input type="text"/>
+                                </label>
+
+                                <div className='check_div'>
+                                    <input type="checkbox" checked='checked' className='check'/>                   
+                                    <label className='newsletter'>Subscribe to our newsletter:</label>
+                                </div>
+                                
+                            </div>
+
+                        <h2 className='sub_title'>Sign-in information</h2>
+
+                            <div className='sign_info'>
+                                <label className='email_signup'>Email:
+                                    <input type="text"
+                                        value={this.state.email}
+                                        onChange={this.handleInput('email')}
+                                    />
+                                </label>
+
+                                <label className='password_signup'>Password:
+                                    <input type="password"
+                                        value={this.state.password}
+                                        onChange={this.handleInput('password')}
+                                    />
+                                </label>
+                            </div>   
+
+                            <div className='enter_back'>
+
+                                <button type="submit" value="Create An Account" className='signupButton'>Create An Account</button>
+                                {this.props.formType === "login"
+                                    ? <Link to='/signup'>Create an account</Link>
+                                    : <Link to='/login' className='backLogin'>Back</Link>
+                                }
+
+                            </div>
+                        </form>
+                    </div>
+
+
+
+                </div>
             </div>
         )
     }
