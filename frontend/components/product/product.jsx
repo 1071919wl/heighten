c988fe5696bc695837491d2aef1a03e36f89f899
William Leung <w.leung.93@gmail.com>
2020-12-17 13:55:56 -0800

    adding search bar

--- frontend/components/product/product.jsx
+++ frontend/components/product/product.jsx
@@ -7,7 +7,8 @@ class Product extends React.Component {
 
         this.state = {
             quantity: 0,
-            //! redirect: null,
+            info: 1,
+            // redirect: null,
         }
         
     }   
@@ -26,6 +27,129 @@ class Product extends React.Component {
     cartValidate(product) {
         if(this.state.quantity > 0){
             return localStorage.setItem(product.id, JSON.stringify({ 'quantity': this.state.quantity, 'object': product }))
+        }
+    }
+
+    handleAddedInfo(){
+        if(this.state.info === 1){
+            return (
+                <div>
+                    <div className='otherContainer'>
+                        <div>
+                            <h2>Wire management grommets</h2>
+                            <p>
+                                Wire management grommets are either situated in the center or on the back left and right corners of your desktop. The drilled openings are circular and measure 80mm (approximately 3.15") in diameter. The covers measure 95mm (3.75") across.
+                                </p>
+                            <h2>
+                                Power upgrade
+                                </h2>
+                            <p>
+                                We also offer powered grommets, which are also available in black or white. Our powered grommets include one AC outlet, one USB-C and one USB-A port, so you can plug in up to 3 devices per grommet. You have the option upgrade one or both of your grommets, or not at all.
+                                For easy desktop access to extra outlets and ports, try our Clamp-Mounted Power Strip. Fully compatible with 3 outlets, one USB-C and one USB-A charging ports, this convenient little power source is designed to give you all the power you need without having to crawl under your desk or add grommet holes to your desktop.
+                                </p>
+                        </div>
+                        <div>
+                            <img src={window.other1URL} />
+                        </div>
+                    </div >
+                    <div className='other2Container'>
+                        <div>
+                            <img src={window.other2URL} />
+                        </div>
+                        <div>
+                            <h2>Designed for supported standing</h2>
+                            <p>
+                                Supported-standing chairs put you in a leaning position relieves lower back pain caused by sitting on your tailbone and gives the right amount of support to activate your body’s natural ability to find balance.
+                                Luna - The charming and affordable Luna stool brings healthy alignment, support, and motion to your adjustable-height desk.
+                                ILOA - A true saddle chair, opens angle of hips to encourage your spine into a natural curve.
+                                </p>
+                        </div>
+                    </div>
+                    <div className='other3Container'>
+                        <div>
+                            <h2>Handset options</h2>
+                            <p>
+                                Jarvis is available with our Up-down toggle handset or an upgraded OLED touch programmable handset option with a height indicator screen. The Up-down toggle handset leverages natural arm motion for intuitive adjustment: press down to lower and lift up to raise it. Our OLED touch programmable handset improves access to the customizable features and functions of your Jarvis.
+                                </p>
+                            <ul>
+                                <li>
+                                    Up-down toggle handset: 1" x 1"
+                                    </li>
+                                <li>
+                                    OLED touch programmable handset: 1.3" x 6"
+                                    </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <img src={window.other3URL} />
+                        </div>
+                    </div>
+                    <div className='other4Container'>
+                        <div>
+                            <img src={window.other4URL} />
+                        </div>
+                        <div>
+                            <h2>Balance boards</h2>
+                            <h3>
+                                Floatdeck Balance Board
+                                </h3>
+                            <p>
+                                Crafted for effortless, intuitive motion, Floatdeck keeps your joints loose and body aligned, while subtly strengthening your stabilizing muscles (core, hips, knees) during long periods of standing.
+                                </p>
+                            <ul>
+                                <li>
+                                    29.5" wide x 12.2" deep
+                                    </li>
+                            </ul>
+                            <h3>
+                                Tic Toc Stand
+                                </h3>
+                            <p>
+                                Developed around the principles that movement is healthy, Tic Toc Stand is designed not so much to make you move, as to move with you when you naturally shift your weight side to side—or front to back, if you want to mix things up.
+                                </p>
+                            <ul>
+                                <li>
+                                    17.6" wide x 11.5" deep
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            )
+        }else if (this.state.info === 2){
+            return (
+                <div>
+                    <div className='otherContainer'>
+                        <div>
+                            <h2>Warranty</h2>
+                            <ul>
+                                <li>10 year warranty on all desk frame components, mechanical parts, motors, and electrical components</li>
+                                <li>5 year warranty on desktop surfaces, excluding normal wear and tear, and damage caused by improper assembly, disassembly or repair</li>
+                            </ul>
+                            
+                            <h2>
+                                Certifications
+                            </h2>
+                            <ul>
+                                <li>BIFMA level® 3 certified</li>
+                                <li>Rated SMaRT© Sustainable Platinum</li>
+                                <li>GREENGUARD and GREENGUARD Gold Certified</li>
+                                <li>Can contribute to U.S. Green Building Council LEED® certification</li>
+                            </ul>
+
+                            <h2>
+                                Shipping Details
+                            </h2>
+                            <ul>
+                                <li>Ships via FedEx Ground from Portland, OR and Nottingham, MD.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            )
         }
     }
 
@@ -100,89 +224,14 @@ class Product extends React.Component {
                 </div>
                 <div>
                     
-                    <h1 className='option_accessory'>Options & Accessories</h1>
+                    <div className='option_accessory'>
+                        <input type='submit' onClick={() => { this.setState({ info: 1}) }} value='Options & Accessories' className='option_accessory1'/>
+                        <input type='submit' onClick={() => { this.setState({ info: 2 }) }} value='Specifications' className='option_accessory2'/>
+                    </div>
 
-                        <div className='otherContainer'>
-                            <div>
-                                <h2>Wire management grommets</h2>
-                                <p>
-                                    Wire management grommets are either situated in the center or on the back left and right corners of your desktop. The drilled openings are circular and measure 80mm (approximately 3.15") in diameter. The covers measure 95mm (3.75") across.
-                                </p>
-                                <h2>
-                                    Power upgrade
-                                </h2> 
-                                <p>
-                                    We also offer powered grommets, which are also available in black or white. Our powered grommets include one AC outlet, one USB-C and one USB-A port, so you can plug in up to 3 devices per grommet. You have the option upgrade one or both of your grommets, or not at all.
-                                    For easy desktop access to extra outlets and ports, try our Clamp-Mounted Power Strip. Fully compatible with 3 outlets, one USB-C and one USB-A charging ports, this convenient little power source is designed to give you all the power you need without having to crawl under your desk or add grommet holes to your desktop.
-                                </p> 
-                            </div> 
-                            <div>
-                                <img src={window.other1URL} />
-                            </div> 
-                        </div >
-                        <div className='other2Container'>
-                            <div>
-                                <img src={window.other2URL} />
-                            </div>
-                            <div>
-                                <h2>Designed for supported standing</h2>
-                                <p>
-                                    Supported-standing chairs put you in a leaning position relieves lower back pain caused by sitting on your tailbone and gives the right amount of support to activate your body’s natural ability to find balance.
-                                    Luna - The charming and affordable Luna stool brings healthy alignment, support, and motion to your adjustable-height desk.
-                                    ILOA - A true saddle chair, opens angle of hips to encourage your spine into a natural curve.
-                                </p>
-                            </div>
-                        </div>
-                        <div className='other3Container'>
-                            <div>
-                                <h2>Handset options</h2>
-                                <p>
-                                    Jarvis is available with our Up-down toggle handset or an upgraded OLED touch programmable handset option with a height indicator screen. The Up-down toggle handset leverages natural arm motion for intuitive adjustment: press down to lower and lift up to raise it. Our OLED touch programmable handset improves access to the customizable features and functions of your Jarvis.
-                                </p>
-                                <ul>
-                                    <li>
-                                        Up-down toggle handset: 1" x 1"
-                                    </li>
-                                    <li>
-                                        OLED touch programmable handset: 1.3" x 6"
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <img src={window.other3URL} />
-                            </div>
-                        </div>
-                        <div className='other4Container'>
-                            <div>
-                                <img src={window.other4URL} />
-                            </div>
-                            <div>
-                                <h2>Balance boards</h2>
-                                <h3>
-                                    Floatdeck Balance Board
-                                </h3>
-                                <p>
-                                    Crafted for effortless, intuitive motion, Floatdeck keeps your joints loose and body aligned, while subtly strengthening your stabilizing muscles (core, hips, knees) during long periods of standing.
-                                </p>
-                                <ul>
-                                    <li>
-                                        29.5" wide x 12.2" deep
-                                    </li>
-                                </ul>
-                                <h3>
-                                    Tic Toc Stand
-                                </h3>
-                                <p>
-                                    Developed around the principles that movement is healthy, Tic Toc Stand is designed not so much to make you move, as to move with you when you naturally shift your weight side to side—or front to back, if you want to mix things up.
-                                </p>
-                                <ul>
-                                    <li>
-                                        17.6" wide x 11.5" deep
-                                    </li>
-                                </ul>
-                            </div>
-                            
-                        </div>
+                        {this.handleAddedInfo()}
+
+                        
 
                 </div>
             </div>
--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -19,7 +19,6 @@ class Search extends React.Component {
     }
 
     handleInput(field) {
-        console.log(this.state);
         return e => {
             this.setState({
                 [field]: e.target.value
@@ -31,81 +30,50 @@ class Search extends React.Component {
     matches(){
         let array = [];
 
-        // console.log('array', this.props.products)
         if (this.state.inputVal.length === 0) {
-            return this.props.products;
+            return [];
         }
 
         this.props.products.forEach(product => {
-            let sub = product.name.slice(0, this.state.inputVal.length);
-            // console.log('sub',sub)
-            if (sub.toLowerCase() === this.state.inputVal.toLowerCase()) {
-                array.push(product.name);
+            let smallWord = product.name.toLowerCase();
+            if (smallWord.includes(this.state.inputVal.toLowerCase())) {
+                array.push(product);
             }
+
         });
 
         if (array.length === 0) {
-            array.push('No array');
+            array.push('No match');
         }
-        // console.log('array' , array)
         return array;
 
     }
-
 
     // selectName(event) {
     //     const productName = event.currentTarget.innerText;
     //     this.setState({ input: productName });
     // }
 
-
-//     render(){
-//         // const results = this.matches().map((result, i) => {
-//         //     return (
-//         //         <li key={i} onClick={this.selectName}>{result.name}</li>
-//         //     );
-//         // });
-        
-//         return (
-//             <div>
-//                 <input value={this.state.input} placeholder='Search for products...' onChange={this.handleInput} />
-//                 {/* {products.length} */}
-
-//                 <ul>
-//                     {this.matches().map((result, i) => 
-//                         <li key={i}>
-//                             {result.name}
-//                         </li>
-//                     )}
-                   
-
-//                 </ul>
-//             </div> 
-
-
-//         )
-//     }
-// }
     render() {
-        // let results = this.matches().map((result, i) => {
-        //     console.log(result.name)
-        //     return (
-        //     <li key={i} className='notWorking'>{result.name}hello</li>
-        //     );
-        // });
-            
+        
         let results = this.matches().map((result, i) => {
-            console.log(result.name)
-            return (
-            <li key={i} className='notWorking'>{result.name}hello</li>
-            );
+            if(result === 'No match'){
+                console.log(result)
+                return (
+                <li key={i} className='notWorking'>There are no results for "{this.state.inputVal}".</li>
+                );
+            }else{
+                return (
+                    <li key={i} className='notWorking'>{result.name}</li>
+                );
+            }
         });
         
         return (
             <div>
                 <h1>Autocomplete</h1>
                 <div>
-                    <input type='text' onChange={this.handleInput('inputVal')} placeholder='Search...' />
+                    <input type='text' onChange={this.handleInput('inputVal')} placeholder='Search for products...' value={this.state.inputVal} />
                     <ul>
                         {results}
                     </ul>
